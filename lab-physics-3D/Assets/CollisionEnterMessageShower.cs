5832fdbd08c5009c29d33ad3b12e1bdabd521b06
Kris Krej <kris@krej.net>
2020-10-09 10:36:13 +0200

    2D and 3D projects

---
+++ lab-physics-3D/Assets/CollisionEnterMessageShower.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionEnterMessageShower : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log(gameObject.name + " has just collided!\n", this);
+    }
+}
---
+++ lab-physics-3D/Assets/EarthQuake.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EarthQuake : MonoBehaviour {
+    Rigidbody[] rigidbodies;
+	
+	void Start () {
+	    rigidbodies = GameObject.FindObjectsOfType<Rigidbody>();
+	}
+	
+	void Update () {
+	    foreach (var rb in rigidbodies) {
+	        rb.AddForce(Random.insideUnitSphere*Time.deltaTime*5000);
+
+        }
+	}
+}
